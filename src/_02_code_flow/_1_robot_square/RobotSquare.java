7568aa4f618f9bc1b2e32a401af85294770c9765
smellstink <54760319+OceanNut420@users.noreply.github.com>
2023-01-21 11:34:10 -0800

    Drew snowman, started robot square.

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -16,22 +16,23 @@ public class RobotSquare {
     	Robot rob = new Robot();
 
         // PEN.  Put the robot's pen down so it can draw, Use this command:
-		 /**     rob.penDown();    **/
+		      rob.penDown();
+		      
 
         // SPEED. Make the robot move quickly. Use this command:
-		  /**     rob.setSpeed(<speed>);    **/
+		      rob.setSpeed(34);    
  
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
-		  /**     int count = 0;    **/
+		       int count = 0;    
     	
 
         // DRAW.  Draw a line. Use this command:
-    		  /**     rob.move(<distance>);    **/
+    		       rob.move(100);    
   
 
 	// TURN.  Turn 90 degrees. Use this command:
-    		  /**     rob.turn(<angle>);    **/
+    		       rob.turn(90);    
 
     	
     	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
